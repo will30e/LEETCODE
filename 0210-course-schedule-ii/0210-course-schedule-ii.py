948a4321c3ab983cfd7799721f4f1a0707ee931d
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-08 01:57:49 -0400

    Time: 96 ms (26.20%), Space: 18.7 MB (14.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,38 +1,50 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Create an adjacency list to store the prerequisites for each course
         prereq = { c : [] for c in range(numCourses)}
 
-        for crs,pre in prerequisites:
+        # Fill the adjacency list with prerequisites
+        for crs, pre in prerequisites:
             prereq[crs].append(pre)
             
-
+        # Output list to store the topological order of courses
         output = []
-        visit, cycle = set() , set()
-
-            
+        # Set to track visited nodes (courses)
+        visit = set()
+        # Set to track nodes in the current path (to detect cycles)
+        cycle = set()
+        
+        # DFS function to perform topological sort
         def dfs(crs):
+            # If the course is already in the cycle set, a cycle is detected
             if crs in cycle:
                 return False
 
+            # If the course is already visited, it means it's already processed
             if crs in visit:
                 return True
-
+            
+            # Mark the course as part of the current cycle
             cycle.add(crs)
+            
+            # Visit all the prerequisites for this course
             for pre in prereq[crs]:
                 if dfs(pre) == False:
                     return False
-
-            cycle.remove(crs)
-            visit.add(crs)
-            output.append(crs)
-            return True
-
-
-        
-        for c in range(numCourses):
-            if dfs(c) == False:
-                return []
-        return output
-
-        
             
+            # Remove from cycle set as we have finished exploring this path
+            cycle.remove(crs)
+            # Mark the course as visited
+            visit.add(crs)
+            # Append the course to the output list (in reverse order of processing)
+            output.append(crs)
+            
+            return True
+        
+        # Call DFS for every course
+        for c in range(numCourses):
+            if not dfs(c):
+                return []  # If a cycle is detected, return an empty list
+        
+        # Return the reversed output list since courses were added in post-order
+        return output
