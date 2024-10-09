37748fbc3041a11660ee3e8bdbf2feafd30ba243
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-09 19:00:59 -0400

    Time: 117 ms (76.92%), Space: 31.1 MB (51.06%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,39 +1,54 @@
-class TrieNode:
+    
+    #Create TRieNode class first with children and a flag to signal when we are at the end of a word
+    #We need a current so we know where we are
+    # each time we insert a word we want to change isword into a True
+    #Each time we look for a word in search we want to return isword
+class TrieNode():
     def __init__(self):
         self.children = {}
         self.isWord = False
-    
-    
 
 class Trie:
+    
 
     def __init__(self):
         self.root = TrieNode()
+        
 
     def insert(self, word: str) -> None:
         cur = self.root
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
+        
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
         cur.isWord = True
+        
 
     def search(self, word: str) -> bool:
         cur = self.root
-        for c in word:
-            if c not in cur.children:
+        
+        for w in word:
+            if w not in cur.children:
                 return False
-            cur = cur.children[c]
+            cur = cur.children[w]              
         return cur.isWord
+        
+        
+                
+        
 
     def startsWith(self, prefix: str) -> bool:
         cur = self.root
-        for c in prefix:
-            if c not in cur.children:
+        
+        for p in prefix:
+            if p not in cur.children:
                 return False
-            cur = cur.children[c]
+            
+            cur = cur.children[p]
+                
         return True
-
+            
 
 
 # Your Trie object will be instantiated and called as such:
