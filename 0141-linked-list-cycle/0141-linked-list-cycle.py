3106dbe09108f0990ac58da362f622824589c294
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-09 18:12:54 -0400

    Time: 49 ms (29.99%), Space: 19.1 MB (69.06%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,11 +6,18 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        #we will check if the head is None
+        #Create 2 pointers s,f
+        # if they are not the same then we will move both of them
+        #s will be twice as slow as fast
+
         if head == None:
             return False
         
         s = head
         f = head.next
+        
         
         while s != f:
             if f == None or f.next == None:
