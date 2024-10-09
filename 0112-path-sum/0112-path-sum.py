e42eb45b7219408294df1388cc496d9c3c42ad21
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-09 18:07:40 -0400

    Time: 42 ms (48.21%), Space: 17.6 MB (13.07%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -4,20 +4,32 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        #Call Dfs on the root and have a current sum dfs will return a bool
+        #if that node is false we will return False
+        #else we would add the node.val to the CurSum
+        #if there are no left and right node then we know we are at the end
+        #if we are at the end then we need to check if the current sum is equal to the target sum
         
         def dfs(node, curSum):
-            if not node:
+            if node == None:
                 return False
             
             curSum += node.val
             
             if not node.left and not node.right:
-                return curSum == targetSum
-            
-            
-            return (dfs(node.left, curSum) or dfs(node.right,curSum))
+                if curSum == targetSum:
+                    return True
+                return False
+                
+                
+            return (dfs(node.left,curSum) or dfs(node.right,curSum))
+                
             
         return dfs(root,0)
+            
+
         
