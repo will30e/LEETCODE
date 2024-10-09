8b28a9f34bdd2285cc992e13ba0068f5c16733da
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-09 19:13:36 -0400

    Time: 117 ms (76.92%), Space: 31.6 MB (23.33%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,29 +1,28 @@
     
-    #Create TRieNode class first with children and a flag to signal when we are at the end of a word
+    #Create TrieNode class first with children and a flag to signal when we are at the end of a word
     #We need a current so we know where we are
-    # each time we insert a word we want to change isword into a True
-    #Each time we look for a word in search we want to return isword
+    # each time we insert a word we want to check if that word is a child of current if not we add it in.
+    # Then we increment cur to the next child and change isword into a True
+    #Each time we look for a word in search we want to see if that word is inside of children if not return false imediately
+    
 class TrieNode():
     def __init__(self):
         self.children = {}
         self.isWord = False
-
 class Trie:
-    
 
     def __init__(self):
         self.root = TrieNode()
-        
 
     def insert(self, word: str) -> None:
         cur = self.root
         
-        for char in word:
-            if char not in cur.children:
-                cur.children[char] = TrieNode()
-            cur = cur.children[char]
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+            
         cur.isWord = True
-        
 
     def search(self, word: str) -> bool:
         cur = self.root
@@ -31,11 +30,9 @@ class Trie:
         for w in word:
             if w not in cur.children:
                 return False
-            cur = cur.children[w]              
+            cur = cur.children[w]
+            
         return cur.isWord
-        
-        
-                
         
 
     def startsWith(self, prefix: str) -> bool:
@@ -44,11 +41,9 @@ class Trie:
         for p in prefix:
             if p not in cur.children:
                 return False
-            
             cur = cur.children[p]
-                
         return True
-            
+        
 
 
 # Your Trie object will be instantiated and called as such:
