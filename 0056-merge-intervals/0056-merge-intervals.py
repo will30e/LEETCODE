bd4770db65190939ae6c02976060c7c9f1aa08b1
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-09 17:02:49 -0400

    Time: 116 ms (88.66%), Space: 21.5 MB (5.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,26 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # First, sort the intervals by the starting time of each interval.
-        # Sorting ensures that overlapping intervals are adjacent.
-        intervals.sort(key=lambda i: i[0])
-
-        # Initialize the 'output' list with the first interval.
+        #sort the intervals
+        #create an output with the first interval inside
+        #check for every start end interval starting from the second interval
+        #get the last end 
+        #if the lastend is <= current start then we will change the outputend output[-1][1]  by getting the max 
+        #between the lastend and current end
+        
+        intervals.sort(key = lambda i : i[0])
         output = [intervals[0]]
-
-        # Iterate through the rest of the intervals (starting from the second one)
-        for start, end in intervals[1:]:
-            # Get the end of the last interval in the output list
+        
+        for start,end in intervals[1:]:
             lastEnd = output[-1][1]
-
-            # Check if the current interval overlaps with the last interval in the output list
+            
             if start <= lastEnd:
-                # If it overlaps, merge the intervals by updating the end of the last interval
-                # to the maximum end time between the two overlapping intervals
-                output[-1][1] = max(lastEnd, end)
+                output[-1][1] = max(lastEnd,end)
             else:
-                # If it does not overlap, add the current interval to the output list
-                output.append([start, end])
-
-        # Return the merged list of intervals
+                output.append([start,end])
+            
         return output
         
