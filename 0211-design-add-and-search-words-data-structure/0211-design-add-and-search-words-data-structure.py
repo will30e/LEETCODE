13d3bd1f9cacc4e1f3c07fe322a04b1cc0bd621c
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-10 14:26:58 -0400

    Time: 1623 ms (41.97%), Space: 64.5 MB (30.06%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,70 +1,47 @@
-# Define the TrieNode class
-class TrieNode:
+class TrieNode():
     def __init__(self):
-        # Each node will have a dictionary of children (to store the next characters)
-        self.children = {}  
-        # This flag marks whether the current node is the end of a word
-        self.endOfWord = False  
+        self.children = {}
+        self.endOfWord = False
 
-# Define the WordDictionary class
 class WordDictionary:
-    
+
     def __init__(self):
-        # Initialize the Trie with an empty root node
         self.root = TrieNode()
 
-    # Method to add a word to the Trie
+
     def addWord(self, word: str) -> None:
-        # Start at the root node
         cur = self.root
         
-        # Traverse each character in the word
-        for c in word:
-            # If the character is not already a child of the current node, create a new node
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            # Move to the next node (corresponding to the character)
-            cur = cur.children[c]
-        
-        # Once all characters are added, mark the last node as the end of the word
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
         cur.endOfWord = True
 
-    # Method to search for a word in the Trie
-    # This method supports searching with '.' as a wildcard (which can match any character)
     def search(self, word: str) -> bool:
         
-        # Depth-First Search (DFS) helper function to recursively search through the Trie
-        def dfs(j, root):
-            # Start at the current node (root in the first call)
+        
+        def dfs(j,root):
             cur = root
 
-            # Loop through the characters in the word starting from index j
             for i in range(j, len(word)):
                 c = word[i]
 
-                # If the current character is a '.', it acts as a wildcard
-                # We need to explore all possible children at this point
                 if c == ".":
-                    # Loop through all children nodes of the current node
-                    for child in cur.children.values():
-                        # Recursively search the rest of the word starting from the next character (i + 1)
-                        if dfs(i + 1, child):
-                            return True  # If any path returns True, the search is successful
-                    # If no paths return True, return False (no match found for the wildcard)
+                    for c in cur.children.values():
+                        if dfs(i + 1, c):
+                            return True
                     return False
                 else:
-                    # If the current character is not in the children, return False (no match)
                     if c not in cur.children:
                         return False
-                    # Move to the next node (corresponding to the current character)
                     cur = cur.children[c]
-            
-            # After the loop, check if we are at a node that is the end of a word
             return cur.endOfWord
-
-        # Start the DFS from the root node, beginning at index 0 of the word
-        return dfs(0, self.root)
-
+                        
+        return dfs(0,self.root)
+                        
+            
+        
 
 
 # Your WordDictionary object will be instantiated and called as such:
