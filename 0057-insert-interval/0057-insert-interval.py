bb71c6daeef0d2f812a3edd5ebea042d471e47b6
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-12 18:17:07 -0400

    Time: 70 ms (84.00%), Space: 20 MB (27.13%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,32 +1,28 @@
 class Solution:
-    #start with a result
-    #loop through all the intervals by range
-    #if the 2 values in new intervals is less than the first value in intervals
-    #if the 1 value in new intervals is greater than the second value in the intervals
-    # if both are false meaning no overlap append it to the res
-    #else create a new interval with the first being the minimun between the current interval and the new interval and the second being the maximum between the current interval and the new interval
-    #return the res
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []  # This will store the result.
 
-        # Iterate through all intervals.
-        for i in range(len(intervals)):
-            # Case: New interval ends before the current interval starts.
-            if newInterval[1] < intervals[i][0]:
-                # Add the new interval to the result and return the remaining intervals.
-                res.append(newInterval)
-                return res + intervals[i:]
-
-            # Case: New interval starts after the current interval ends.
-            elif newInterval[0] > intervals[i][1]:
-                # Add the current interval to the result as-is.
-                res.append(intervals[i])
-
-            # Case: New interval overlaps with the current interval.
+        # Insert all intervals, handling overlaps with `newInterval`.
+        for start, end in intervals:
+            # Get the last end of the current result, if any intervals exist.
+            if res:
+                lastEnd = res[-1][1]
             else:
-                # Merge the intervals by taking the minimum start and maximum end.
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                lastEnd = None  # No intervals yet.
 
-        # If the new interval was not added during the loop, add it now.
+            # If the new interval is completely before the current one.
+            if newInterval[1] < start:
+                res.append(newInterval)  # Insert the new interval.
+                return res + intervals[intervals.index([start, end]):]  # Add remaining intervals.
+
+            # If the new interval is after the current one, just add the current interval.
+            elif newInterval[0] > end:
+                res.append([start, end])
+
+            # If there is an overlap, merge the intervals.
+            else:
+                newInterval = [min(newInterval[0], start), max(newInterval[1], end)]
+
+        # If the new interval was not added during the loop, add it at the end.
         res.append(newInterval)
         return res
