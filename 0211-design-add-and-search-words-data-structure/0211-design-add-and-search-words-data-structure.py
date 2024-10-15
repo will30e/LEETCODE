4715708e3f3d1138d4c1915dd6f0cfaadf426f05
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-14 23:53:06 -0400

    Time: 1560 ms (57.29%), Space: 64.3 MB (45.29%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,18 +1,13 @@
-#first build a TrieNode with children and a flag indicating when when we are at the end of the word
-# to add a word into the list put current at the root node and iterate through the words in the root nodes children
-# after set the flag to True after adding the word
-#for search run dfs on it if the 
-
 class TrieNode():
     def __init__(self):
         self.children = {}
-        self.endOfWord = False
+        self.word = False
 
 class WordDictionary:
 
     def __init__(self):
         self.root = TrieNode()
-
+        
 
     def addWord(self, word: str) -> None:
         cur = self.root
@@ -20,8 +15,11 @@ class WordDictionary:
         for w in word:
             if w not in cur.children:
                 cur.children[w] = TrieNode()
+                
             cur = cur.children[w]
-        cur.endOfWord = True
+            
+        cur.word = True
+        
 
     def search(self, word: str) -> bool:
         
@@ -29,25 +27,22 @@ class WordDictionary:
         def dfs(j,root):
             cur = root
 
-            for i in range(j, len(word)):
-                c = word[i]
+            for w in range(j,len(word)):
+                c = word[w]
 
                 if c == ".":
-                    for c in cur.children.values():
-                        if dfs(i + 1, c):
+                    for child in cur.children.values():
+                        if dfs(w + 1, child):
                             return True
                     return False
                 else:
                     if c not in cur.children:
                         return False
                     cur = cur.children[c]
-            return cur.endOfWord
-                        
-        return dfs(0,self.root)
-                        
-            
+            return cur.word
         
-
+        return dfs(0,self.root)
+        
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
