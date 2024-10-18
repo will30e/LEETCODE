aad132c9b9890310774a4416ba4e8ef8530b7e53
William Otoo-Mensah <williammensah2003@gmail.com>
2024-10-18 11:22:14 -0400

    Time: 10 ms (100.00%), Space: 20.8 MB (38.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,11 +4,14 @@ class Solution:
         output = [intervals[0]]
         
         
-        for start, end in intervals[1:]:
+        for start,end in intervals[1:]:
             lastEnd = output[-1][1]
             
             if start <= lastEnd:
-                output[-1][1] = max(lastEnd, end)
+                output[-1][1] = max([lastEnd, end])
+                
             else:
                 output.append([start,end])
-        return output        
+                
+        return output
+        
